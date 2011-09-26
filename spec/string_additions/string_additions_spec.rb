37374bafb9ef3e4b24affd55f52999cf6282b163
Shamaoke <shamaoke@hotmail.com>
2011-09-26 08:35:05 +0400

    refactor example group '#lcolstrip'

--- spec/string_additions/string_additions_spec.rb
+++ spec/string_additions/string_additions_spec.rb
@@ -4,11 +4,11 @@ require 'spec_helper'
 
 describe String do
   describe '#lcolstrip' do
-    let!(:raw_string) do
+    let(:raw_string) do
       "\n      Эй, жлоб!\n    Где туз?\n        Прячь юных съемщиц в шкаф.\n"
     end
 
-    let!(:result) do
+    let(:result) do
       "  Эй, жлоб!\nГде туз?\n    Прячь юных съемщиц в шкаф."
     end
 
